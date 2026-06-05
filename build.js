77602c876ace8cabb3a6f9f34cc422bb7abd3e06
Andrechotech <69160191+andresdiazf@users.noreply.github.com>
2026-06-05 00:00:10 -0500

    ajuste orden scripts

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const fs = require('fs');
 
-let config = fs.readFileSync('../../js/config.example.js', 'utf8');
+let config = fs.readFileSync('./js/config.js', 'utf8');
 
 config = config
-  .replace('TU_CLOUD_NAME', process.env.CLOUDINARY_CLOUD_NAME)
-  .replace('TU_UPLOAD_PRESET', process.env.CLOUDINARY_UPLOAD_PRESET);
+  .replace('%%CLOUDINARY_CLOUD_NAME%%', process.env.CLOUDINARY_CLOUD_NAME)
+  .replace('%%CLOUDINARY_UPLOAD_PRESET%%', process.env.CLOUDINARY_UPLOAD_PRESET);
 
-fs.writeFileSync('../../js/config.js', config);
+fs.writeFileSync('./js/config.js', config);
 console.log('✅ config.js generado correctamente');
