7f4c535672093a65ec8c47d47a879032ae631d83
Andrechotech <69160191+andresdiazf@users.noreply.github.com>
2026-06-05 09:54:40 -0500

    validacion de errorres

--- build.js
+++ build.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const fs = require('fs');
 
-// ✅ Lee el EJEMPLO, no el config final
+// Lee el EJEMPLO, no el config final
 let config = fs.readFileSync('./js/config.example.js', 'utf8');
 
 config = config
   .replace('TU_CLOUD_NAME', process.env.CLOUDINARY_CLOUD_NAME)
   .replace('TU_UPLOAD_PRESET', process.env.CLOUDINARY_UPLOAD_PRESET);
 
-// ✅ Genera el config.js final
+//  Genera el config.js final
 fs.writeFileSync('./js/config.js', config);
-console.log('✅ config.js generado correctamente');
+console.log('config.js generado correctamente');
